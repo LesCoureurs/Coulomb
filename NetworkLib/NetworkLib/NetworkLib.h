30e4602415f4b5ef7a0a314f48067c14a8ea6819
ianngiaw <ianngiawtingan@gmail.com>
2016-03-14 20:44:46 +0800

    Initialized base repository

---
+++ NetworkLib/NetworkLib/NetworkLib.h
@@ -0,0 +1,19 @@
+//
+//  NetworkLib.h
+//  NetworkLib
+//
+//  Created by Ian Ngiaw on 3/14/16.
+//  Copyright © 2016 nus.cs3217.group5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkLib.
+FOUNDATION_EXPORT double NetworkLibVersionNumber;
+
+//! Project version string for NetworkLib.
+FOUNDATION_EXPORT const unsigned char NetworkLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkLib/PublicHeader.h>
+
+
