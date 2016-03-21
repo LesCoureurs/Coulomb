ddd4e8581ba62788cac8c2a0fac9e427d8ee00f6
joeleba <hieu.giang.2810@gmail.com>
2016-03-21 21:03:03 +0800

    deinit method

--- NetworkLib/NetworkLib/SplooshSearch.swift
+++ NetworkLib/NetworkLib/SplooshSearch.swift
@@ -52,7 +52,8 @@ public class SplooshSearch: NSObject {
     }
     
     deinit {
-        stopPeerSearch()
+        stopAdvertisingHost()
+        stopSearchingForHosts()
     }
     
     // MARK: Methods for host
