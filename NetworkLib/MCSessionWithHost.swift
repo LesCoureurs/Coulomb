1c9be6dfc4c91293c0c48afefbfea9bfc78e3abd
joeleba <hieu.giang.2810@gmail.com>
2016-03-25 17:21:14 +0800

    new class MCSessionWithHost

---
+++ NetworkLib/MCSessionWithHost.swift
@@ -0,0 +1,24 @@
+//
+//  MCSessionWithHost.swift
+//  NetworkLib
+//
+//  Created by Hieu Giang on 25/3/16.
+//  Copyright © 2016 nus.cs3217.group5. All rights reserved.
+//
+
+import MultipeerConnectivity
+class MCSessionWithHost: MCSession {
+    var host: MCPeerID?
+    
+    func assignNextHostInLine() {
+        guard host == nil else {
+            return
+        }
+        
+        guard !connectedPeers.isEmpty else {
+            return
+        }
+        
+        host = connectedPeers[0]
+    }
+}
